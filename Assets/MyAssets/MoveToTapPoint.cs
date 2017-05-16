01efb561d14b9bedd55e1791b3a0b788e7cf1e6b
Hiroki Tanaka <tanaka.hiroki@gmail.com>
2017-05-16 21:51:30 +0900

    first commit

---
+++ Assets/MyAssets/MoveToTapPoint.cs
@@ -0,0 +1,48 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTapPoint : MonoBehaviour, IInputClickHandler
+{
+	public NavMeshAgent Agent;
+	LineRenderer lineRenderer;
+
+	void Start()
+	{
+		InputManager.Instance.PushFallbackInputHandler(gameObject);
+		Agent.gameObject.SetActive(false);
+		lineRenderer = gameObject.GetComponent<LineRenderer>();
+		lineRenderer.positionCount = 0;
+	}
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+		if (GazeManager.Instance.IsGazingAtObject)
+		{
+			var hitInfo = GazeManager.Instance.HitInfo;
+			if (!Agent.gameObject.activeSelf)
+			{
+				Agent.gameObject.SetActive(true);
+				Agent.transform.position = hitInfo.point;
+			}
+			else
+			{
+				Agent.destination = hitInfo.point;
+
+				// パスの計算
+				var path = new NavMeshPath();
+				NavMesh.CalculatePath(Agent.transform.position, Agent.destination, NavMesh.AllAreas, path);
+				var positions = path.corners;
+
+				// ルートの描画
+				lineRenderer.widthMultiplier = 0.1f;
+				lineRenderer.positionCount = positions.Length;
+				for (int i = 0; i < positions.Length; i++)
+				{
+					Debug.Log("point " + i + "=" + positions[i]);
+					lineRenderer.SetPosition(i, positions[i]);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/MyAssets/UnityChanAnimate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnityChanAnimate : MonoBehaviour {
+
+	NavMeshAgent agent;
+	Animator animator;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () {
+		var speed = agent.velocity.magnitude / transform.localScale.y;
+		animator.SetFloat("Speed", speed);
+	}
+	
+}
