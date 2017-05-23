2f4f296f1c643b06058e6f9e51112a38f6a8ba5f
Hiroki Tanaka <tanaka.hiroki@gmail.com>
2017-05-23 18:18:42 +0900

    PC

---
+++ Assets/MyAssets/other/TapToShow.cs
@@ -0,0 +1,23 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TapToShow : MonoBehaviour, IInputClickHandler
+{
+	public GameObject bullet;
+
+	void Start()
+	{
+		InputManager.Instance.PushFallbackInputHandler(gameObject);
+	}
+
+	public void OnInputClicked(InputClickedEventData eventData)
+	{
+		if (GazeManager.Instance.IsGazingAtObject)
+		{
+			GameObject go = Instantiate (bullet);
+			Rigidbody rb = go.GetComponent<Rigidbody> ();
+			rb.AddForce (new Vector3 (0f, 0f, 100f));
+		}
+	}
+}
